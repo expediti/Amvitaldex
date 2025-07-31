5ab83dd1893dad4da5ca2c32de34f55fd0b76070
expediti <153554895+expediti@users.noreply.github.com>
2025-07-31 21:43:37 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { FiArrowLeft, FiActivity } from 'react-icons/fi';
+import { useNavigate, useLocation } from 'react-router-dom';
 import '../styles/Header.css';
 
-const Header = ({ onBackToHome, showBack }) => {
+const Header = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  const handleBackToHome = () => {
+    navigate('/');
+  };
+
   return (
     <header className="header">
       <div className="container">
         <div className="header-content">
-          {showBack && (
-            <button className="back-button" onClick={onBackToHome}>
+          {!isHomePage && (
+            <button className="back-button" onClick={handleBackToHome}>
               <FiArrowLeft size={24} />
             </button>
           )}
           
-          <div className="logo">
+          <div className="logo" onClick={handleBackToHome} style={{cursor: 'pointer'}}>
             <FiActivity className="logo-icon" />
             <h1 className="logo-text">VitalDex</h1>
           </div>
